8c6b381ef3850788f6c28478793fc02899164f74
Kevan Hollbach <khollbach@gmail.com>
2026-06-17 16:26:36 -0400

    Tidy find

--- user/find.c
+++ user/find.c
@@ -5,16 +5,13 @@
 #include "kernel/fcntl.h"
 
 void find(char *path, char *name) {
-    char buf[512], *p;
     int fd;
-    struct dirent de;
-    struct stat st;
-
     if ((fd = open(path, O_RDONLY)) < 0) {
         fprintf(2, "find: cannot open %s\n", path);
         return;
     }
 
+    struct stat st;
     if (fstat(fd, &st) < 0) {
         fprintf(2, "find: cannot stat %s\n", path);
         close(fd);
@@ -23,42 +20,48 @@ void find(char *path, char *name) {
 
     switch (st.type) {
         case T_DEVICE:
-            close(fd);
-            return;
-        case T_FILE:
+        case T_FILE: {
             // Find first character after last slash.
-            for(p=path+strlen(path); p >= path && *p != '/'; p--) {}
+            char *p;
+            for (p = path + strlen(path); p >= path && *p != '/'; p--);
             p++;
             if (strcmp(p, name) == 0) {
                 printf("%s\n", path);
             }
+        } break;
 
-            close(fd);
-            return;
-
-        case T_DIR:
-            if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+        case T_DIR: {
+            char buf[512];
+            if (strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf)) {
                 printf("find: path too long\n");
                 break;
             }
             strcpy(buf, path);
-            p = buf+strlen(buf);
+
+            char *p = buf + strlen(buf);
             *p++ = '/';
-            while(read(fd, &de, sizeof(de)) == sizeof(de)){
-                if (de.inum == 0) continue;
+
+            struct dirent de;
+            while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+                if (de.inum == 0) {
+                    continue;
+                }
                 if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0) {
                     continue;
                 }
+
                 memmove(p, de.name, DIRSIZ);
                 p[DIRSIZ] = 0;
-                if(stat(buf, &st) < 0){
+
+                if (stat(buf, &st) < 0) {
                     printf("find: cannot stat %s\n", buf);
                     continue;
                 }
                 find(buf, name);
             }
-            break;
+        } break;
     }
+
     close(fd);
 }
 
