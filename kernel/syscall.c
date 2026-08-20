58e8a7f4d6846ce29614dd9b7661cb21f4243945
Kevan Hollbach <khollbach@gmail.com>
2026-08-20 13:18:50 -0400

    Speed up system calls

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -105,9 +105,9 @@ extern uint64 sys_close(void);
 #ifdef LAB_NET
 extern uint64 sys_connect(void);
 #endif
-#ifdef LAB_PGTBL
+// #ifdef LAB_PGTBL
 extern uint64 sys_pgaccess(void);
-#endif
+// #endif
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -136,9 +136,9 @@ static uint64 (*syscalls[])(void) = {
 #ifdef LAB_NET
 [SYS_connect] sys_connect,
 #endif
-#ifdef LAB_PGTBL
+// #ifdef LAB_PGTBL
 [SYS_pgaccess] sys_pgaccess,
-#endif
+// #endif
 };
 
 
